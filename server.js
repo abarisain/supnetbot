f3ef460bef9a9b07163b384c9a060ca2be2cd06b
Arnaud Barisain-Monrose <abarisain@gmail.com>
2016-02-03 21:17:40 +0100

    Add backends handling. Can't use destructuration yet :(

--- server.js
+++ server.js
@@ -2,11 +2,33 @@
 
 "use strict";
 
-const config = require('./config');
 const logger = require('winston');
 
-const readline = require('readline');
+const config = require('./config');
+const messagesHandler = require('./messages_handler');
 
 logger.level = config.log_level;
 
 logger.info("Starting supnetbot ´･ω･｀(what a shitty name)");
+
+//TODO: Maybe get this out of server.js
+
+const backends = {
+  "terminal": require('./backends/terminal')
+};
+
+const enabledBackends = {};
+
+for (var backendName of backends) {
+  let backendConfig = config.backends[backendName];
+  if (typeof backendConfig !== "object") {
+    logger.error("Backend " + backendName + "isn't configured. Skipping.");
+    continue;
+  }
+  if (backendConfig.enabled === true) {
+    logger.info("Loading backend: " + backendName);
+    let instance = new backends[backendName]();
+    enabledBackends[backendName] = instance;
+    messagesHandler.registerBackend(instance);
+  }
+}
