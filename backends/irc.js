bd61a8c05bd7bd1b26f9df5fc76bd900889d815c
Arnaud Barisain-Monrose <abarisain@gmail.com>
2016-02-06 15:14:51 +0100

    IRC Backend

--- backends/irc.js
+++ backends/irc.js
@@ -3,10 +3,61 @@
 const MessagesHandler = require('../messages_handler');
 const AbstractBackend = require('./abstract_backend');
 const logger = require('winston');
+const IRCClient = require('irc');
 
 /**
- *
+ * IRC Backend
  */
 class IRC extends AbstractBackend {
 
+    constructor(options) {
+        super(options);
+
+        if (options.server === undefined) {
+            throw new Error("[IRC] - Server wasn't configured");
+        }
+
+        if (options.channel === undefined) {
+            throw new Error("[IRC] - Channel wasn't configured");
+        }
+
+        if (options.nickname === undefined) {
+            throw new Error("[IRC] - Nickname wasn't configured");
+        }
+
+        this.ircClient = null;
+
+        /**
+         * @type {string}
+         */
+        this.server = options.server;
+
+        /**
+         * @type {string}
+         */
+        this.channel = options.channel;
+
+        /**
+         * @type {string}
+         */
+        this.nickname = options.nickname;
+    }
+
+    connect() {
+        this.ircClient = new IRCClient.Client(this.server, this.nickname, {
+            "channels": [this.channel]
+        });
+
+        this.ircClient.addListener('message', function (from, to, message) {
+            console.log(from + ' => ' + to + ': ' + message);
+        });
+    }
+
+    get name() {
+        return "irc";
+    }
+
+    send(message) {
+        this.ircClient.say(this.channel, message);
+    }
 }
--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
             "enabled": false,
             "server": "localhost:6667",
             "password": null,
-            "channel": "#supbottest", //use channel:password if you need one
+            "channel": "#supbottest", //use "channel password" if you need one
             "nickname": "JeanYves"
         },
         "discord": {
