8a67919ca50cece04945fc900bb32b5f476747fd
Arnaud Barisain-Monrose <arnaud@batch.com>
2017-07-06 00:09:12 +0200

    Actually I wanted harmony

--- bin/supnetbot.js
+++ bin/supnetbot.js
@@ -1,4 +1,4 @@
 #!/bin/bash
-":" //# http://sambal.org/?p=1014; exec /usr/bin/env node --noharmony "$0" "$@"
+":" //# http://sambal.org/?p=1014; exec /usr/bin/env node --harmony "$0" "$@"
 
 require("../lib/server").start(require("./config"));
